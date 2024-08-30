0c40f4871ae286a4c00ca66974b41f9cc55ec00e
Daniel Boadzie <boadziedaniel43@gmail.com>
2024-08-30 14:14:31 +0000

    Connect HTMX to project
    - Add htmx to get endpoint to display message

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+from pathlib import Path
+
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from fastapi_htmx import htmx, htmx_init
 
 app = FastAPI(title="Keyman")
+htmx_init(templates=Jinja2Templates(directory=Path(".") / "templates"))
 
 
-@app.get("/")
-async def root():
+@app.get("/", response_class=HTMLResponse)
+@htmx("index", "index")
+async def root(request: Request):
     return {"message": "Keyman is running!"}
 
 
